3362bb2fa782354b7cf5b45936ef3baf8d1a5efc
david <dc.romao@campus.fct.unl.pt>
2018-04-06 17:25:32 +0100

    Added test script and changed servers uri from localhost to 0.0.0.0. Created placeholder class ComputeNodeServer

---
+++ src/sys/mapreduce/ComputeNodeServer.java
@@ -0,0 +1,7 @@
+package sys.mapreduce;
+
+public class ComputeNodeServer {
+    public static void main(String[] args) {
+        //does nothing, just placeholder
+    }
+}
--- src/test/DatanodeClientTest.java
+++ src/test/DatanodeClientTest.java
@@ -7,7 +7,7 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DatanodeClientTest {
 
